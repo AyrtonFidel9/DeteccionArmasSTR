cd4bf0a9d02d5013bafe382f00ec4962915bd010
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-02-10 12:20:42 -0500

    cambios para el boton iniciar

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@
 const socket = io();
 
 const btnIniciar = document.getElementById('iniciar');
+const notification = document.getElementById('notificacion');
 btnIniciar.disabled = true;
 var init = false;
 
@@ -13,23 +14,13 @@ socket.on('disconnect', ()=>{
     console.log("Live stream vigilancia conectado");
 })
 
-socket.on('video', (image) => {
-    console.log(socket.id)
-    console.log(image.data)
-    if(init)
-    {
-        var img = document.getElementById('play');
-        img.src = "data:image/jpg;base64,"+image.data;
-    }
-})
 
 socket.on('notification', (value)=>{
     if(value.msg){
         btnIniciar.disabled = false;
         document.getElementById('not').style.display ="block"
-        document.getElementById('not').innerText = "!!!Arma detectada!!!";
     }
-})
+});
 
 function detener()
 {
@@ -40,11 +31,20 @@ function reanudar()
 {
     init = True;
     socket.connect();
+    socket.on('video', (image) => {
+        console.log(socket.id)
+        console.log(image.data)
+        if(init)
+        {
+            var img = document.getElementById('play');
+            img.src = "data:image/jpg;base64,"+image.data;
+        }
+    })
 }
 
 function cerrar()
 {
-    document.getElementById('not').style.display ="none"
+    document.getElementById('not').style.display ="none";
 }
 
 
