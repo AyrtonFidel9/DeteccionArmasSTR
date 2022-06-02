015c1d754294897e165c60df6ed9d6418df19128
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-06-01 19:20:27 -0500

    Se especifico la version de la libreria imutils

--- test/decodificarVideoTest.py
+++ test/decodificarVideoTest.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch
 
-from DeteccionArmasSTR.control import decodificarVideo
+from src.control import decodificarVideo
 
 @patch("control.decodificarVideo", return_value="[0,1,2]")
 def test_decodificarvideo(patched):
