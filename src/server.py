1898621560d9f2941e13e1235d733c75fcc15c80
Edwin Manzano <edwinmanzanoquinzo@gmail.com>
2022-06-02 23:43:52 -0500

    Se modificaron las pruebas unitarias del servidor: msg, connect y disconnect

--- src/server.py
+++ src/server.py
@@ -9,25 +9,47 @@ app.wsgi_app = socketio.WSGIApp(sio, static_files={
     '/': "./public/"
 })
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @sio.event
 def connect(sid, environ):
-    print("connect ", sid)
+    try:
+        if sid is None or environ is None:
+            raise Exception("The connection failed!")
+        else:
+            print("connect ", sid)
+    except Exception as e:
+        return str(e)
+
 
 @sio.event
 def msg(sid, data):
-    print(sid, " message ", data)
-    sio.emit('notification', {'msg':data})
+    try:
+        if sid is None or data is None:
+            raise  Exception('Error')
+        else:
+            print(sid, " message ", data)
+            sio.emit('notification', {'msg': data})
+    except Exception as e:
+        return str(e)
 
 @sio.event
 def disconnect(sid):
-    print('disconnect ', sid)
+    try:
+        if sid is None:
+            raise Exception('error')
+        else:
+            print('disconnect ', sid)
+    except Exception as e:
+        return str(e)
+
 
 @sio.event
-def livestream(sid,video):
+def livestream(sid, video):
     # print(sid," frames: ",video)
     sio.emit('video', {'data': video})
 
--- src/server_test.py
+++ src/server_test.py
@@ -1,16 +1,40 @@
 import pytest
+
 from server import *
 
-def test_connectparametro():
-    sid="id"
-    environ = "http://localhost:8080"
-    assert connect(sid, environ) == None
 
-def test_desconectado():
-    sid = "id"
-    assert disconnect(sid) == None
+@pytest.mark.parametrize(
+    ["sid", "environ", "salida"],
+    [
+        ('id', 'http://localhost:8080', None),
+        (None, None, 'The connection failed!'),
+        ('id', None, 'The connection failed!'),
+        (None, 'http://localhost:8080', 'The connection failed!')
+    ]
+)
+def test_connectparametro(sid, environ, salida):
+    assert connect(sid, environ) == salida
 
-def test_msg():
-    sid="id"
-    data="imagen"
-    assert msg(sid,data)==None
+
+@pytest.mark.parametrize(
+    ["sid", "salida"],
+    [
+        ('id', None),
+        (None, 'error')
+    ]
+)
+def test_desconectado(sid, salida):
+    assert disconnect(sid) == salida
+
+
+@pytest.mark.parametrize(
+    ["sid", "data", "salida"],
+    [
+        ('id', 'imagen', None),
+        (None, None, 'Error'),
+        ('id', None, 'Error'),
+        (None, 'imagen', 'Error')
+    ]
+)
+def test_msg(sid, data, salida):
+    assert msg(sid, data) == salida
